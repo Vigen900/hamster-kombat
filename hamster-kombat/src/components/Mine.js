9bc9f20aa0240fa94d7467f1a544d464f95edb42
Vigen900 <mamon90009@gmail.com>
2024-07-23 17:54:54 +0400

    show real cards

--- hamster-kombat/src/components/Mine.js
+++ hamster-kombat/src/components/Mine.js
@@ -4,14 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "../mine.css"
 import {useState} from 'react'
 function Mine(){
-    const tasks = [
-        {cost:1_000_000, img:'', title:'Ceo', profite:1_700, level:2, type:'Pr&Team'},
-        {cost:1_000_000, img:'', title:'Market', profite:1_700, level:2, type:'Market'},
-        {cost:1_000_000, img:'', title:'Legal', profite:1_700, level:2, type:'Legal'},
-        {cost:1_000_000, img:'', title:'Specialist', profite:1_700, level:2, type:'Specialist'},
-        {cost:1_000_000, img:'', title:'Specialist', profite:1_700, level:2, type:'Specialist'},
-        {cost:1_000_000, img:'', title:'Marketing', profite:1_700, level:2, type:'Pr&Team'},
-    ]
+    const [tasks, setTasks] = useState([])
+        fetch('http://localhost:3001/cards').then((data)=>{
+            return data.json()
+        })
+        .then((data)=>{
+            setTasks(data)
+        }).catch((error)=>{
+            console.log(error)
+        })
     
     const [selectedType, setSelectedType] = useState('Pr&Team')
     function handleChange(type){
@@ -41,8 +42,11 @@ function Mine(){
             </div>
             <div className="parent">        
                     {tasks.map((u)=>{
-                        return u.type == selectedType && <div className="card">{u.title}{u.level}</div>
-                    })}
+                        return (u.type == selectedType && 
+                        <div className="card">{u.title}{u.level}
+                            <img src={u.image} width={'100%'}/>
+                        </div>
+                    )})}
             </div>
             <Navigation selected={'mine'}></Navigation>
         </div>
