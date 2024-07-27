37013b36bf3188b0299a557f3580504504152aed
Vigen900 <mamon90009@gmail.com>
2024-07-27 19:28:07 +0400

    added cards function

--- hamster-kombat/src/components/Mine.js
+++ hamster-kombat/src/components/Mine.js
@@ -3,14 +3,24 @@ import { faDollarSign } from '@fortawesome/free-solid-svg-icons/faDollarSign'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "../mine.css"
 import {useState, useEffect} from 'react'
-function Mine(){
+function Mine({selected}){
+    const [cards, setCards] = useState([])
     const [tasks, setTasks] = useState([])
+    const [userData, setUserData] = useState({})
     useEffect(()=>{
-        fetch('http://localhost:3001/cards').then((data)=>{
+        const cardRequest = fetch('http://localhost:3001/cards').then((data)=>{
             return data.json()
         })
-        .then((data)=>{
-            setTasks(data)
+        const userRequest = fetch('http://localhost:3001/users/66890cb072ef7a31a76e0645').then((data)=>{
+            return data.json()
+          })
+          Promise.all([cardRequest, userRequest])
+        .then(([cards, userData])=>{
+            setCards(cards)
+            setUserData(userData)
+            const tasks = transformCards(cards, userData)
+            console.log(tasks)
+            setTasks(tasks)
         }).catch((error)=>{
             console.log(error)
         })
@@ -20,18 +30,45 @@ function Mine(){
     function handleChange(type){
         setSelectedType(type)
     }
+
+    function transformCards(cards, userData){
+        const tasks = []
+        cards.forEach((card)=>{
+          const userCard = userData.cards.find((element)=> element.id == card._id)
+          const task = {
+            id: card._id,
+            image: card.image,
+            type: card.type,
+            level: getNextCard(card._id, userCard?.level || 0 , cards),
+          }
+          tasks.push(task)
+          console.log(task)
+        })
+        return tasks
+    }
+
+    function getNextCard(id, currentLevel, cards) {
+
+        const card =  (cards.find((element)=> element._id == id))
+        if (!card){
+            return
+        }
+        const level = (card.levels.find((element)=> element.level == currentLevel + 1))
+        return level
+    }
+
     return<>
         <div className="App">
             <div className="total-coin">
                 <FontAwesomeIcon className='faDollarSign' icon={faDollarSign}/>
-                <b>20.000.000</b>
+                {userData.profite}
             </div>
             <div className="type-of-market">
                 <ul>
-                    <li className="pr-team" onClick={()=>{handleChange('Pr&Team')}}>
+                    <li className={selected == 'Pr&Team'}  onClick={()=>{handleChange('Pr&Team')}}>
                         PR&Team
                     </li>
-                    <li onClick={()=>{handleChange('Market')}}>
+                    <li className={selected == 'Market'} onClick={()=>{handleChange('Market')}}>
                         Market
                     </li>
                     <li onClick={()=>{handleChange('Legal')}}>
@@ -45,8 +82,11 @@ function Mine(){
             <div className="parent">        
                     {tasks.map((u)=>{
                         return (u.type == selectedType && 
-                        <div className="card">{u.title}{u.level}
+                        <div className="card">{u.title}<br/>
                             <img src={u.image} width={'100%'}/>
+                            Level: {u.level.level} <br/>
+                            Profite: {u.level.profite} <br/>
+                            Cost: {u.level.cost}
                         </div>
                     )})}
             </div>
@@ -54,5 +94,6 @@ function Mine(){
         </div>
     </>
 }
+
 
 export default Mine;
